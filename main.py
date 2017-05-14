6cd582b14aa2667aca613addd87c54509b94664f
Tony <lin1000@gmail.com>
2017-05-14 16:41:09 +0800

    add visualization and vis.js for social community graph

--- main.py
+++ main.py
@@ -12,14 +12,24 @@ def hello():
     """Return a friendly HTTP greeting."""
     return '<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script> Hello <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="i-chien-tony-lin-b49ba012"><a class="LI-simple-link" href=\'https://tw.linkedin.com/in/i-chien-tony-lin-b49ba012?trk=profile-badge\'>I-Chien (Tony) Lin</a></div>'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/googleea2cd7e0a312099b.html')
 def google_search_console():
     """Return a google_search_console verification code"""
     return 'google-site-verification: googleea2cd7e0a312099b.html'
+
+@app.route('/visualization_overview')
+def visualization_overview():
+    return render_template('visualization_overview.html')
+
+@app.route('/social_community_graph')
+def social_community_graph():
+    """Demonstrating social community graphs."""
+    return render_template('social_community_graph.html')
+
+@app.route('/social_community_graph/vis_network')
+def social_community_graph_vis_network():
+    """Demonstrating social community graphs."""
+    return render_template('social_community_graph_vis_network.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
